7a05386726329f6b45e0ed71b7675d1383c6a765
Akhila mitta <akhilamitta@gmail.com>
2022-11-29 09:32:48 -0600

    made chnages

--- models/Europe.js
+++ models/Europe.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose")
 const EuropeSchema = mongoose.Schema({
-    countries: String,
-    state: String,
-    ranking: Number
+    countries: {
+        type:String,
+        required:true
+    },
+    state: {
+        type:String,
+        required:true
+    },
+    ranking: {
+        type:Number,
+        min:1,
+        max:500
+    }
 })
-module.exports = mongoose.model("Europe", EuropeSchema)
+module.exports = mongoose.model("Europe",
+    EuropeSchema)
--- routes/Europe.js
+++ routes/Europe.js
@@ -2,6 +2,16 @@ var express = require('express');
 const Europe_controlers= require('../controllers/Europe'); 
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  }
+
 // /* GET home page. */
 
 // router.get('/', function (req, res, next) {
@@ -14,11 +24,11 @@ router.get('/detail', Europe_controlers.Europe_view_one_Page);
 router.get('/', Europe_controlers.Europe_view_all_Page ); 
 router.get('/Europes/:id', Europe_controlers.Europe_detail); 
 /* GET create Europe page */
-router.get('/create', Europe_controlers.Europe_create_Page);
+router.get('/create',secured, Europe_controlers.Europe_create_Page);
 /* GET create update page */
-router.get('/update', Europe_controlers.Europe_update_Page);
+router.get('/update',secured, Europe_controlers.Europe_update_Page);
 /* GET delete Europe page */
-router.get('/delete', Europe_controlers.Europe_delete_Page);
+router.get('/delete',secured, Europe_controlers.Europe_delete_Page);
 
 module.exports = router;
 
